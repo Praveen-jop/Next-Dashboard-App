0c1e6efa1a9fff4ae038c43a8cac24e5e0f2a932
PRAVEEN-JO <115061255+Praveen-jop@users.noreply.github.com>
2025-01-13 00:43:54 +0530

    update

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,3 @@
 import { Inter } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
-
-// import { Lusitana } from 'next/font/google';
-
-// export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
